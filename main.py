33f671de8b09bd474dba57cedb977f41f9d2f1ed
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2022-12-11 14:06:17 +0300

    зубодробительный хуесос

--- main.py
+++ main.py
@@ -31,14 +31,11 @@ def bebra_delete(id):
             cursor.execute(f'delete from agricultural_machinery where id_agricultural_machinery = {id}')
 
 
-
-def convert(sql_result):
-    result = []
-    for i in sql_result:
-        result.append({"id": i[0], "type": i[1], "title": i[2], "content": i[3], "lastSentAt": i[4]})
-    return result
+def bebra_put(id):
+    with pymssql.connect("46.39.232.190", "TestUser", "vag!nA228##", "Agronomic_App_TestUser") as conn:
+        with conn.cursor(as_dict=True) as cursor:
+            cursor.execute(f'delete from agricultural_machinery where id_agricultural_machinery = {id}')
 
 
 if __name__ == '__main__':
     bebra(1)
-
